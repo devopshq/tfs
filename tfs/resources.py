e7a28335e72b0de2f2a81cedc8e30bdffbd59ab0
Aleksey Burov <allburov@gmail.com>
2017-10-11 11:50:08 +0700

    Test system key IN dict

--- tfs/resources.py
+++ tfs/resources.py
@@ -52,7 +52,8 @@ class Workitem(TFSObject):
         self.id = self._data['id']
 
     def __setitem__(self, key, value):
-        field_path = "/fields/{}{}".format(self._system_prefix, key)
+        result_key = key if key in self._fields else self._system_prefix + key
+        field_path = "/fields/{}".format(result_key)
         update_data = [dict(op="add", path=field_path, value=value)]
         raw = self.tfs.update_workitem(self.id, update_data)
         self.__init__(raw, self.tfs)
