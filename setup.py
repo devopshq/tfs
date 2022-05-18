b5c68c739a75aefeca473a5e383167e4ddd6390d
allburov <allburov@gmail.com>
2022-05-18 17:21:27 +0700

    Use fixed version in setup.py - 1.1.1

--- setup.py
+++ setup.py
@@ -1,41 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-
 from setuptools import setup
-
-__version__ = "1.0"  # identify main version of dohq-tfs tool
-devStatus = "4 - Beta"  # default build status, see: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-
-if "TRAVIS_BUILD_NUMBER" in os.environ and "TRAVIS_BRANCH" in os.environ:
-    print("This is TRAVIS-CI build")
-    print("TRAVIS_BUILD_NUMBER = {}".format(os.environ["TRAVIS_BUILD_NUMBER"]))
-    print("TRAVIS_BRANCH = {}".format(os.environ["TRAVIS_BRANCH"]))
-
-    __version__ += ".{}{}".format(
-        ""
-        if "release" in os.environ["TRAVIS_BRANCH"]
-        or os.environ["TRAVIS_BRANCH"] == "master"
-        else "dev",
-        os.environ["TRAVIS_BUILD_NUMBER"],
-    )
-
-    devStatus = (
-        "5 - Production/Stable"
-        if "release" in os.environ["TRAVIS_BRANCH"]
-        or os.environ["TRAVIS_BRANCH"] == "master"
-        else devStatus
-    )
-
-else:
-    print("This is local build")
-    __version__ += ".dev0"  # set version as major.minor.localbuild if local build: python setup.py install
-
-print("dohq-tfs build version = {}".format(__version__))
 
 setup(
     name="dohq-tfs",
-    version=__version__,
+    version="1.1.1",
     description="Microsoft TFS Python Library (TFS API Python client) that can work with TFS workflow and workitems.",
     long_description="You can see detailed user manual here: https://devopshq.github.io/tfs/",
     license="MIT",
@@ -45,8 +14,7 @@ setup(
     download_url="https://github.com/devopshq/tfs.git",
     entry_points={},
     classifiers=[
-        "Development Status :: {}".format(devStatus),
-        "Environment :: Console",
+        "Development Status :: 5 - Production/Stable" "Environment :: Console",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: MIT License",
